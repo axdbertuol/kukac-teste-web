33e3181c2158795349a5f52945961a6d70d362cb
Alexandre Bertuol <axdber@gmail.com>
2022-05-24 13:15:13 -0300

    1st

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main className="h-screen w-2/3 mx-auto">
+        <div className="flex flex-col items-center">
+
+        </div>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
